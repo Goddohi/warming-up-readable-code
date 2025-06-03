e78919e8e4772bf65dad2791696820654ee484b6
kyungtaeChoi <kmam147@gmail.com>
2025-06-04 05:52:46 +0900

    리팩토링 : selectedpass의 과정을 메소드화

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -20,16 +20,7 @@ public class StudyCafePassMachine {
         try {
             outputHandler.showIntro();
 
-            StudyCafePassType studyCafePassType = inputHandler.getPassTypeSelectingUserAction();
-
-            List<StudyCafePass> studyCafePasses = studyCafeFileHandler.readStudyCafePasses();
-
-            List<StudyCafePass> typePasses = studyCafePasses.stream()
-                    .filter(studyCafePass -> studyCafePass.getPassType() == studyCafePassType)
-                    .toList();
-
-            outputHandler.showPassListForSelection(typePasses);
-            StudyCafePass selectedPass = inputHandler.getSelectPass(typePasses);
+            StudyCafePass selectedPass = getStudyCafePass();
 
             StudyCafeLockerPass lockerPass = getStudyCafeLockerPass(selectedPass);
 
@@ -39,6 +30,20 @@ public class StudyCafePassMachine {
         } catch (Exception e) {
             outputHandler.showSimpleMessage("알 수 없는 오류가 발생했습니다.");
         }
+    }
+
+    private StudyCafePass getStudyCafePass() {
+        StudyCafePassType studyCafePassType = inputHandler.getPassTypeSelectingUserAction();
+
+        List<StudyCafePass> studyCafePasses = studyCafeFileHandler.readStudyCafePasses();
+
+        List<StudyCafePass> typePasses = studyCafePasses.stream()
+                .filter(studyCafePass -> studyCafePass.getPassType() == studyCafePassType)
+                .toList();
+
+        outputHandler.showPassListForSelection(typePasses);
+        StudyCafePass selectedPass = inputHandler.getSelectPass(typePasses);
+        return selectedPass;
     }
 
     private StudyCafeLockerPass getStudyCafeLockerPass(StudyCafePass selectedPass) {
